fb067c49f64452c32744b2d698d91887dc4f4bb7
Вадим Малутин <malutin.vadim@yandex.ru>
2022-02-01 13:50:32 +0600

    Add project files.

---
+++ Lab2.19/Models/Geom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2._19.Models
+{
+    class Geom
+    {
+        public static double CircLen(int r) => 2 * Math.PI * r;
+    }
+}
---
+++ Lab2.19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Lab2._19.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab2._19.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]string PropertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private int radius;
+        public int Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double circleLength;
+        public double CircleLength
+        {
+            get => circleLength;
+            set
+            {
+                circleLength = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+
+        private void OnAddCommandExecute(object p)
+        {
+            CircleLength = Geom.CircLen(Radius);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+            if (Radius != 0)
+                return true;
+            else
+                return false;
+        }
+
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
